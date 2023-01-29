e1170ac53f3c6f1d36da5b787674b5a13499876d
Abdessamad DERRAZ <derraz.abdessamad@gmail.com>
2023-01-29 21:58:40 +0100

    Import LUIS Stream Module

--- dialogs/__init__.py
+++ dialogs/__init__.py
@@ -5,5 +5,6 @@ from .booking_dialog import BookingDialog
 from .cancel_and_help_dialog import CancelAndHelpDialog
 from .date_resolver_dialog import StrDateResolverDialog, EndDateResolverDialog
 from .main_dialog import MainDialog
+from .texttoluisprompt import TextToLuisPrompt
 
-__all__ = ["BookingDialog", "CancelAndHelpDialog", "StrDateResolverDialog", "EndDateResolverDialog", "MainDialog"]
+__all__ = ["BookingDialog", "CancelAndHelpDialog", "StrDateResolverDialog", "EndDateResolverDialog", "MainDialog", "TextToLuisPrompt"]
