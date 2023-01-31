2a91482655966d84badada92935e5b0998341294
Abdessamad DERRAZ <derraz.abdessamad@gmail.com>
2023-01-31 17:00:23 +0100

    LUIS Stream: Better budget management

--- dialogs/texttoluisprompt.py
+++ dialogs/texttoluisprompt.py
@@ -1,64 +1,64 @@
-import typing
+from typing import Dict
 
-from botbuilder.dialogs.prompts import Prompt, PromptOptions, PromptRecognizerResult
 from botbuilder.core.turn_context import TurnContext
+from botbuilder.dialogs.prompts import Prompt, PromptOptions, PromptRecognizerResult
 from botbuilder.schema import ActivityTypes
 
-from flight_booking_recognizer import FlightBookingRecognizer
 from config import DefaultConfig
-from helpers.luis_helper import LuisHelper, Intent
+from flight_booking_recognizer import FlightBookingRecognizer
+
 
 class TextToLuisPrompt(Prompt):
     def __init__(
-        self,
-        dialog_id: str,
-        luis_recognizer: FlightBookingRecognizer = FlightBookingRecognizer(DefaultConfig),
-        validator: typing.Callable[[str], str] = None
+            self,
+            dialog_id: str,
+            luis_recognizer: FlightBookingRecognizer = FlightBookingRecognizer(DefaultConfig),
+            validator=None,
     ):
         self.dialog_id = dialog_id
         self.luis_recognizer = luis_recognizer
-        self.detected_intent = None
         super().__init__(dialog_id, validator=validator)
 
     async def on_prompt(
-        self,
-        turn_context: TurnContext, 
-        state: dict, 
-        options: PromptOptions, 
-        is_retry: bool,
+            self,
+            turn_context: TurnContext,
+            state: Dict[str, object],
+            options: PromptOptions,
+            is_retry: bool,
     ):
         if not turn_context:
-            raise TypeError("turn_context can't be None")
+            raise TypeError("turn_context cannot be None")
         if not options:
-            raise TypeError("options can't be None")
+            raise TypeError("options cannot be None")
 
-        if is_retry and options.retry_prompt:
+        if is_retry and options.retry_prompt is not None:
             await turn_context.send_activity(options.retry_prompt)
-        elif options.prompt:
-            await turn_context.send_activity(options.prompt)   
-                
-    async def on_recognize(self, turn_context: TurnContext, state: dict, options: PromptOptions) -> PromptRecognizerResult:  
+        elif options.prompt is not None:
+            await turn_context.send_activity(options.prompt)
+
+    async def on_recognize(
+            self,
+            turn_context: TurnContext,
+            state: Dict[str, object],
+            options: PromptOptions,
+    ) -> PromptRecognizerResult:
         if not turn_context:
-            raise TypeError("turn_context can't be None")
+            raise TypeError("turn_context cannot be None")
 
         if turn_context.activity.type != ActivityTypes.message:
             return PromptRecognizerResult(succeeded=False)
 
-        usertext = turn_context.activity.text
-        prompt_result = PromptRecognizerResult()
-        recognizer_result = await self.luis_recognizer.recognize(turn_context)
-        entities = recognizer_result.entities.get("$instance", {})
-        is_valid = ["geographyV2" in key for key in entities.keys()]
-        entity = entities.get(self.dialog_id, [])
-        if len(entity) > 0 and True in is_valid:
-            entity = entity[0]["text"].capitalize()
-        elif (self.dialog_id == "or_city" or self.dialog_id == "dst_city") and "geographyV2" in recognizer_result.entities:
-            entity = recognizer_result.entities["geographyV2"][0]["text"].capitalize()
-        else:
-            prompt_result.succeeded = False
-            return prompt_result
+        user_text = turn_context.activity.text
+        luis_result = await self.luis_recognizer.recognize(turn_context)
+        entities = luis_result.entities.get("$instance", {})
 
-        prompt_result.succeeded = True
-        prompt_result.value = entity
+        entity = None
+        if self.dialog_id == "budget" and "money" in luis_result.entities:
+            entity = f"{luis_result.entities['money'][0]['number']} {luis_result.entities['money'][0]['units']}"
 
-        return prompt_result
+        elif self.dialog_id in ["or_city", "dst_city"] and "geographyV2_city" in entities:
+            entity = entities["geographyV2_city"][0]["text"].title()
+
+        if entity:
+            return PromptRecognizerResult(succeeded=True, value=entity)
+        return PromptRecognizerResult(succeeded=False)
